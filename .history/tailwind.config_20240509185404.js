45bd4ac77efaa8f82e77cb4470e63c1d3af677bb
zzm <zzm12990@gmail.com>
2024-05-20 18:52:50 +0800

    init

---
+++ .history/tailwind.config_20240509185404.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        primary:'#FF6363',
+        secondary:'#888883',
+      }
+    },
+  },
+  plugins: [],
+}
+
